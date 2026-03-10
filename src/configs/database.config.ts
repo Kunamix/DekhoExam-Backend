fb45477f8444b4abb81e238b669898721a758786
Kunal <kunal34255@gmail.com>
2026-03-10 21:40:09 +0530

    Change database provider from PostgreSQL to MySQL and simplify Prisma client initialization

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,20 +1,11 @@
 import { myEnvironment } from "@/configs/env.config";
 import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-  connectionString: myEnvironment.DATABASE_URL,
-});
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (myEnvironment.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
